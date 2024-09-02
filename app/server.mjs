d4e990e75bb5dccc0c67c25cb99df9f6e6b43fbb
Elfen20 <elfen20@outlook.de>
2024-09-02 18:21:14 +0200

    + infos, fixes

--- app/server.mjs
+++ app/server.mjs
@@ -7,7 +7,7 @@
 
 import path from 'node:path';
 import config from 'config';
-const serverCfg = config.has('server') ? config.server : { host: '0.0.0.0', port: 8888};
+const serverCfg = config.has('server') ? config.server : { host: '0.0.0.0', port: 8888 };
 import log from './lib/logger.mjs';
 log.info('- - - - Logging started - - - - - - - - - - - - - - - - - - - -');
 
@@ -18,12 +18,11 @@ import Handlebars from 'handlebars';
 
 import blinkenLedList from './lib/blinkenledlist.mjs';
 import udpServer from './lib/udpserver.mjs';
-
-//const redis = require('./lib/redis');
+import blinkenLedDefs from './lib/blinkenleddefs.mjs';
 
 log.debug('pre-init done');
 
-const fastify = Fastify({logger: {level: 'warn', }});
+const fastify = Fastify({ logger: { level: 'warn', } });
 
 // register static rendering
 fastify.register(fastifyStatic, {
@@ -44,30 +43,46 @@ fastify.register(fastifyView, {
 fastify.get('/log', async (request, reply) => {
   // sample = await redis.client.json.get('sample_restaurant:10');
   sample = "123"
-  return reply.viewAsync('log.hbs', { logItems: [], sample: JSON.stringify(sample)});
+  return reply.viewAsync('log.hbs', { logItems: [], sample: JSON.stringify(sample) });
 })
 
 fastify.get('/scan', async (request, reply) => {
   udpServer.server.sendCommand('224.0.0.237', 'HELO');
-  return reply.send({ result: 'Ok'});
+  return reply.send({ result: 'Ok' });
 })
 
 fastify.get('/bled/:bledId', async (request, reply) => {
   const bledIp = blinkenLedList.getItemIp(request.params.bledId);
-  const response = await fetch(`http://${bledIp}/info`).catch((error) => logger.error(error));
-  const info = await response.text();
-  return reply.viewAsync('bled.hbs', { bledIp: bledIp, info: JSON.stringify(info, null, '  ') });
+  const response = await fetch(`http://${bledIp}/info`).catch((error) => log.error(error));
+  const config = await response.json();
+  return reply.viewAsync('bled.hbs', {
+    bledId: request.params.bledId,
+    bledDefs: blinkenLedDefs,
+    bledIp: bledIp,
+    bledConfig: config,
+    bledCText: JSON.stringify(config, null, '  ')
+  });
 })
 
 fastify.get('/update/:bledId', async (request, reply) => {
   blinkenLedList.updateBLed(request.params.bledId);
-  return reply.send({ result: 'Ok'});
+  return reply.send({ result: 'Ok' });
 })
+
+fastify.get('/command/:bledId/:command/*', async (request, reply) => {
+  const params = [request.params.command];
+  params.push(...request.params['*'].split('/'));
+  const bledIp = blinkenLedList.getItemIp(request.params.bledId);
+  log.info(`Sending command to ${bledIp} : ${JSON.stringify(params)}`);
+  udpServer.sendCommand(bledIp, ...params);
+  return reply.send({ result: 'Ok' });
+})
+
 
 
 fastify.get('/', async (request, reply) => {
   const blItems = blinkenLedList.getItemsJson();
-  log.info(JSON.stringify(blItems));
+  log.debug(JSON.stringify(blItems));
   return reply.viewAsync('index.hbs', { blItems: blinkenLedList.getItemsJson() });
 })
 
